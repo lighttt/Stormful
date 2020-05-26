60f85780cbf269c1a51d78c49bc7457cb5266585
Manish Tuladhar <manish.eclipse@gmail.com>
2020-05-26 10:30:22 +0545

    Added loading indicator and menu button for refresh

--- app/src/main/java/com/example/trystromful/MainActivity.java
+++ app/src/main/java/com/example/trystromful/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.trystromful;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.trystromful.data.StormfulPreferences;
 import com.example.trystromful.utilities.NetworkUtils;
 import com.example.trystromful.utilities.OpenWeatherJsonUtils;
+import com.wang.avi.AVLoadingIndicatorView;
 
 import java.net.URL;
 
@@ -17,12 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
     private static final String TAG = "MainActivity";
+    private TextView mErrorMessageDisplay;
+    private AVLoadingIndicatorView mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
         mWeatherTextView = findViewById(R.id.tv_weather_data);
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
         loadWeatherData();
     }
 
@@ -31,7 +40,28 @@ public class MainActivity extends AppCompatActivity {
         new FetchWeatherTask().execute(location);
     }
 
+    private void showWeatherDataView() {
+        /* First, make sure the error is invisible */
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        /* Then, make sure the weather data is visible */
+        mWeatherTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+        /* First, hide the currently visible data */
+        mWeatherTextView.setVisibility(View.INVISIBLE);
+        /* Then, show the error */
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
     public class FetchWeatherTask extends AsyncTask<String,Void,String[]>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
         @Override
         protected String[] doInBackground(String... params) {
             if (params.length == 0) {
@@ -54,11 +84,37 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] weatherData) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
             if (weatherData != null) {
                 for (String weatherString : weatherData) {
                     mWeatherTextView.append((weatherString) + "\n\n\n");
                 }
             }
+            else{
+                showErrorMessage();
+            }
         }
+    }
+
+    /*
+    *  ----------------------  Menu Items --------------------------
+    * */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forecast,menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+       int id = item.getItemId();
+       if(id == R.id.action_refresh)
+       {
+           mWeatherTextView.setText("");
+           loadWeatherData();
+           return  true;
+       }
+       return super.onOptionsItemSelected(item);
     }
 }
